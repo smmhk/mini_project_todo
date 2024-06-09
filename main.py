d5505e5e4db211e7b622b913deeebb715c3dcd1f
Kento Kitasuga <hokutonokento0706@gmail.com>
2024-06-09 00:47:48 -0700

    Add some functions

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 def print_menu():
-    print("To-Do List Application")
-    print("1. Add Task")
-    print("")
-    print("")
-    print("")
+    print('To-Do List Application\n1. Add Task\n2. Remove Task\n3. View Tasks\n4. Exit')
 
+task_list = ['apple', 'banana', 'pear']
 
-print_menu()
+def add_task():
+    task = input("Enter the task: ")
+    task_list.append(task)
+    print(f"'{task}' has been added to the list.")
+
+def remove_task():
+    rv_task = input("Enter the task to remove: ")
+    task_list.remove(rv_task)
+    print(f"'{rv_task} has beeb removed from the list.'")
+
+def view_task():
+    print("To-Do List:")
+    for i, list in enumerate(task_list, start=1):
+        print(f"{i}. {list}")
+
+def exit_app():
+    print("Exiting the application. Goodbye!")
